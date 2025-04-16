118a004934f287da1087f45b7685e9c210afa425
Kevin Benton <kevin.benton@cashtn.com>
2025-04-16 09:48:18 -0400

    Added InnoDBCluster as a db_type, but not InnoDBRouter

--- index.php
+++ index.php
@@ -210,7 +210,7 @@ SELECT CONCAT( h.hostname, ':', h.port_number )
   FROM aql_db.host AS h
  WHERE h.decommissioned = 0
    AND h.should_monitor = 1
-   AND h.db_type IN ( 'MySQL', 'MariaDB' )
+   AND h.db_type IN ( 'MySQL', 'MariaDB', 'InnoDBCluster' )
  ORDER BY h.hostname, h.port_number
  
 SQL;
@@ -226,7 +226,7 @@ SELECT CONCAT( h.hostname, ':', h.port_number )
   FROM aql_db.host AS h
  WHERE h.decommissioned = 0
    AND CONCAT( h.hostname, ':', h.port_number ) IN ( $in )
-   AND h.db_type IN ( 'MySQL', 'MariaDB' )
+   AND h.db_type IN ( 'MySQL', 'MariaDB', 'InnoDBCluster' )
  ORDER BY h.hostname, h.port_number
 
 SQL ;
@@ -239,7 +239,7 @@ SELECT hg.host_group_id, hg.tag, CONCAT( '"', GROUP_CONCAT( CONCAT( h.hostname, 
   LEFT
   JOIN host AS h
  USING ( host_id )
- WHERE h.db_type IN ( 'MySQL', 'MariaDB' )
+ WHERE h.db_type IN ( 'MySQL', 'MariaDB', 'InnoDBCluster' )
  GROUP BY hg.host_group_id
  ORDER BY hg.tag
 
